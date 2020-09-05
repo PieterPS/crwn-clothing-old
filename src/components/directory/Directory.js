f8176c9e6d2e3515a35bf020db0afdfb57dca89c
Pieter <pieterpretorius223@gmail.com>
2020-09-05 22:47:18 +0200

    updating menu-item to naviaget withRouter HOC

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -46,8 +46,8 @@ class Directory extends Component {
   render() { 
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id, imageUrl, size }) => {
-          return <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => {
+          return <MenuItem key={id} {...otherSectionProps} />
         })}
       </div>
     );
