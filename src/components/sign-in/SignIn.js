82cfadfe0c8d5b760f6d58747dd8eeadea5c0858
Pieter <pieterpretorius223@gmail.com>
2020-09-06 13:58:20 +0200

    finishing sign in functionality

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/FormInput';
 import CustomButton from '../custom-button/CustomButton';
 
-import { signInWithGoogle } from '../../firebase/firebase';
+import { auth, signInWithGoogle } from '../../firebase/firebase';
 
 import './sign-in.scss';
 
@@ -16,10 +16,17 @@ class SignIn extends Component {
      }
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   handleChange = e => {
